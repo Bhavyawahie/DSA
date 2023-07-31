0418d55bfbf31b6a11869f625b06cbc51246df1f
Bhavya Wahie <bhavyawahie@gmail.com>
2023-07-31 17:01:20 +0530

    updated

--- Practice.java
+++ Practice.java
@@ -45,6 +45,24 @@ class Practice {
             arr[j+1] = temp;
         }
     }
+    public static void coutingSort(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int[] counting = new int[largest+1];
+        for (int i = 0; i < arr.length; i++) {
+            counting[arr[i]]++;
+        }
+        int j = 0;
+        for (int i = 0; i < counting.length; i++) {
+            while(counting[i] > 0) {
+                arr[j] = i;
+                j++;
+                counting[i]--;
+            }
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -55,7 +73,8 @@ class Practice {
         //function call
         // bubbleSort(arr);
         // selectionSort(arr);
-        insertionSort(arr);
+        // insertionSort(arr);
+        coutingSort(arr);
         printArray(arr);
     }
     
