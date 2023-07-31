b526b5d9748a0c1bf9e8751b25cb256228b95148
Bhavya Wahie <bhavyawahie@gmail.com>
2023-07-31 09:45:15 +0530

    bubble sorting problem added

---
+++ SORTING/BubbleSort.java
@@ -0,0 +1,32 @@
+package SORTING;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    arr[j] = arr[j] + arr[j+1];
+                    arr[j+1] = arr[j] - arr[j+1];
+                    arr[j] = arr[j] - arr[j+1];
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        bubbleSort(arr);
+        System.out.print("Sorted Array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
